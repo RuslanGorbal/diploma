2479a5bbeced29c5c0efa55b097c7215c54686af
Руслан Горбаль <ruslangorbal719@gmail.com>
2023-05-03 17:08:09 +0300

    Add list of types of numbers and files of these numbers

--- webhotel/main/urls.py
+++ webhotel/main/urls.py
@@ -5,6 +5,12 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('about', views.about, name='about'),
+    path('about_standart', views.about_standart, name='about_standart'),
+    path('about_deluxe', views.about_deluxe, name='about_deluxe'),
+    path('about_family', views.about_family, name='about_family'),
+    path('about_luxe', views.about_luxe, name='about_luxe'),
+    path('about_proluxe', views.about_proluxe, name='about_proluxe'),
+    path('about_business', views.about_business, name='about_business'),
     path('services', views.services, name='services'),
     path('create', views.create, name='create')
 ]
--- webhotel/main/views.py
+++ webhotel/main/views.py
@@ -11,6 +11,30 @@ def about(request):
     return render(request, 'main/about.html')
 
 
+def about_standart(request):
+    return render(request, 'main/about_standart.html')
+
+
+def about_deluxe(request):
+    return render(request, 'main/about_deluxe.html')
+
+
+def about_family(request):
+    return render(request, 'main/about_family.html')
+
+
+def about_luxe(request):
+    return render(request, 'main/about_luxe.html')
+
+
+def about_proluxe(request):
+    return render(request, 'main/about_proluxe.html')
+
+
+def about_business(request):
+    return render(request, 'main/about_business.html')
+
+
 def services(request):
     tasks = Task.objects.order_by('-id')
     return render(request, 'main/services.html', {'title': 'Послуги', 'tasks': tasks})
